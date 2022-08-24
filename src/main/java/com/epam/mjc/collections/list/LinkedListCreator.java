94a56a0495d64815198384cdf2a4b3a19c4858a4
ilaylyk <ilay_lyk@mail.ru>
2022-08-24 05:38:09 +0300

    collections Task1,2,3

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> createLinkedList = new LinkedList<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if ((sourceList.get(i) % 2) == 0) {
+                createLinkedList.addLast(sourceList.get(i));
+            } else {
+                createLinkedList.addFirst(sourceList.get(i));
+            }
+        }
+        return createLinkedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,29 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        double price1 =  (5*Math.pow(Integer.parseInt(a), 2) +3);
+        double price2 =  (5*Math.pow(Integer.parseInt(b), 2) +3);
+
+        if (price1 > price2) {
+            return 1;
+        } else if (price1 < price2) {
+            return -1;
+        } else if (price1 == price2){
+            return -1;
+        } else {
+            return 0;
+        }
     }
 }
